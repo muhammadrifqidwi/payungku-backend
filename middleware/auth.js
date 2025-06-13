6e2541fa62497c317d77fb3fb707c3c7bdd490f9
muhammadrifqidwi <mrifqidwip90@gmail.com>
2025-06-13 22:32:54 +0700

    update auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+const User = require("../models/user");
 
 // Untuk Vercel: tanpa `next()`, panggil sebagai fungsi biasa
 const verifyToken = async (req, res) => {
