845407a11d0d6359e00eac45e240d824ac015413
muhammadrifqidwi <mrifqidwip90@gmail.com>
2025-06-16 09:26:07 +0700

    cors

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const midtransRoutes = require("./routes/midtransRoutes");
 
 const app = express();
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://payungku.vercel.app/",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
